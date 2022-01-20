e05173886b4b7f8589efe1a5e75b8c5f12a47fcc
pankaj5417 <87115576+pankaj5417@users.noreply.github.com>
2022-01-20 11:26:56 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import "./App.css";
 import Details from "./Components/TermlifeInsurance/Details";
+import UseForm from "./Components/userForm/UseForm";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <Routes>
         {/* all public and private routes be declared here*/}
         <Route path="/" element={<Home />}></Route>
+        <Route path="/user" element={<UseForm /> }></Route>
+
       </Routes>
     </div>
   );
