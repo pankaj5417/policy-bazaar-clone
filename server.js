4bac5e6788d550396b7981fce539632163ea8ec6
pankaj5417 <87115576+pankaj5417@users.noreply.github.com>
2022-02-23 00:30:02 +0530

    make it better

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = jsonServer.create();
 const path = require('path');
 const express = require('express');
 const middlewares = jsonServer.defaults();
-const router = jsonServer.router('userdb.json');
+const router = jsonServer.router('/db.json');
 const port = process.env.PORT || 3001;
 
-app.use('/userdb', middlewares, router);
+app.use('/db', middlewares, router);
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
