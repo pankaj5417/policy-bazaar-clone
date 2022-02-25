ffe87fa2465c0652d67014e6b20be8c4662aa4d8
pankaj5417 <87115576+pankaj5417@users.noreply.github.com>
2022-02-25 15:39:21 +0530

    make it better

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const middlewares = jsonServer.defaults();
 const router = jsonServer.router('db.json');
 const port = process.env.PORT || 8000;
 
-// app.use('/db', middlewares,router);
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use('/db', middlewares,router);
+app.use(express.static(path.join(__dirname, 'build')));
 
-// app.get('/*', function (req, res) {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 
